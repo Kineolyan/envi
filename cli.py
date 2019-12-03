a893fced6e4f3ad8162ec8d0afcf00a19afffa3d
Olivier Peyrusse <ope@activeviam.com>
2019-12-03 13:36:52 +0100

    Add options to list all envs.

--- cli.py
+++ cli.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from pathlib import Path
 import sys
 
 from __version import VERSION
@@ -9,25 +10,44 @@ def parse_arguments(args):
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "action", 
-        choices=["current", "version", "shell"], 
+        choices=["current", "version", "shell", "list"], 
         help="Possible actions")
     parser.add_argument(
         "env_name",
-        action="store_const",
-        const=None)
+        default=None,
+        nargs="?",
+        help="Name of the environment to consider")
     return parser.parse_args(args)
+
+
+def list_envs():
+        root = Path.home() / ".config" / "envi"
+        print(f"Looking into `{root}`")
+        if not root.exists():
+            print("No defined environments")
+
+        count = 0
+        for file in root.glob("*.ini"):
+            name = str(file.name).replace(".ini", "")
+            print(f" - {name}")
+            count += 1
+
+        if count == 0:
+            print("No defined environments")
 
 
 def main():
     args = parse_arguments(sys.argv[1:])
     if args.action == "version":
         print(f"Version {VERSION}")
-    if args.action == "current":
+    elif args.action == "current":
         env_name = os.getenv("ENVI_ENV_NAME", "(None)")
         print(f"Current env: {env_name}")
-    if args.action == "shell":
+    elif args.action == "shell":
         forwarded_args = [arg for arg in sys.argv[1:] if arg != "shell"]
         entrypoint.generate(forwarded_args)
+    elif args.action == "list":
+        list_envs()
 
 
 if __name__ == "__main__":
